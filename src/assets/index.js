cdcadd0ca2ed9bd03e166c62896bae8f6c2b09ac
UMANG KIRIT LODAYA <lodayaumang71@gmail.com>
2023-09-18 16:10:42 +0530

    Final Commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,41 +1,41 @@
-import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import logo from "/assets/logo.png";
+import backend from "/assets/backend.png";
+import creator from "/assets/creator.png";
+import mobile from "/assets/mobile.png";
+import web from "/assets/web.png";
+import github from "/assets/github.png";
+import menu from "/assets/menu.svg";
+import close from "/assets/close.svg";
 
-import hackerrank from './hackerrank.png'
-import leetcode from './leetcode.png'
-import kaggle from './kaggle.png'
-import linkedin from './linkedin.png'
-import gfg from './gfg.png'
+import hackerrank from '/assets/hackerrank.png'
+import leetcode from '/assets/leetcode.png'
+import kaggle from '/assets/kaggle.png'
+import linkedin from '/assets/linkedin.png'
+import gfg from '/assets/gfg.png'
 
-import cpp from "./tech/cpp.png"
-import css from "./tech/css.png";
-import excel from "./tech/excel.png"
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import java from "./tech/java.png"
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import mysql from "./tech/mysql.png"
-import nodejs from "./tech/nodejs.png";
-import python from "./tech/python.png"
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
+import cpp from "/assets/tech/cpp.png"
+import css from "/assets/tech/css.png";
+import excel from "/assets/tech/excel.png"
+import figma from "/assets/tech/figma.png";
+import git from "/assets/tech/git.png";
+import html from "/assets/tech/html.png";
+import java from "/assets/tech/java.png"
+import javascript from "/assets/tech/javascript.png";
+import mongodb from "/assets/tech/mongodb.png";
+import mysql from "/assets/tech/mysql.png"
+import nodejs from "/assets/tech/nodejs.png";
+import python from "/assets/tech/python.png"
+import reactjs from "/assets/tech/reactjs.png";
+import tailwind from "/assets/tech/tailwind.png";
+import threejs from "/assets/tech/threejs.svg";
 
-import gochanakya from "./company/gochanakya.jpg";
-import suvidha from "./company/suvidha.jpg";
+import gochanakya from "/assets/company/gochanakya.jpg";
+import suvidha from "/assets/company/suvidha.jpg";
 
-import stock from "./stock.png";
-import neural from "./neural.png";
-import customer from "./customer.png";
-import recommendation from "./recommendation.png";
+import stock from "/assets/stock.png";
+import neural from "/assets/neural.png";
+import customer from "/assets/customer.png";
+import recommendation from "/assets/recommendation.png";
 
 export {
   logo,
